cb81482fe2401c516ee41111f426061f741b6204
Anandhu <anandhureji33@gmail.com>
2022-08-04 22:05:05 +0530

    clear single item added

--- src/reducer.js
+++ src/reducer.js
@@ -1,8 +1,17 @@
+import CartItem from "./CartItem"
+
 const reducer =(state,action)=>{
 
     if(action.type==='CLEAR_CART')
     {
         return {...state,cart:[]}
+    }
+
+    if(action.type==='REMOVE')
+    {
+        return{
+            ...state,cart:state.cart.filter((cartItem)=>cartItem.id!==action.payload),
+        }
     }
     return state
 }
